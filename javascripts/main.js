1605206019387420262e1145ab2f17d35c8342f8
Jack Groetzinger <jack@seatgeek.com>
2013-01-09 21:49:42 -0500

    A few visual tweaks

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $("#one").fadeIn("normal", function(){
   $("#two").fadeIn("normal", function(){
     $("#three").css({display:'block'}).animate({top:'190px', opacity:'1'},350, function(){
-      $("#four").css({display:'block'}).animate({bottom:'29px', opacity:'1'},300);
+      $("#four").css({display:'block'}).animate({bottom:'20px', opacity:'1'},300);
     });
   });
 });
